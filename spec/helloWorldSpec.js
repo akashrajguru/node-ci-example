3067e077ff3a871741df145db7a339e558a0fd6f
Akash Rajguru <akashrajguru@icloud.com>
2016-11-04 16:43:40 +0000

    commit 3

--- spec/helloWorldSpec.js
+++ spec/helloWorldSpec.js
@@ -15,29 +15,33 @@ describe("Hello World Server", function () {
        it("should return Hello World", function (done) {
            request.get(base_url, function (error, response, body) {
                expect(body).toBe("Hello World");
-               done();
+                done();
            });
        });
    });
+
     /*sub test suite*/
     describe("GET /number/3", function () {
-       it("should return status code 200", function () {
-            request.get(base_url+'number/3', function (error, response, body) {
+        it("should return status code 200", function (done) {
+            request.get(base_url + 'number/3', function (error, response, body) {
                 expect(response.statusCode).toBe(200);
-            });
-       });
+                done();
 
-        it("should return array with length of 3", function () {
-            request.get(base_url+'number/3', function (error, response, body) {
-                expect(body.length).toBe(2);
             });
         });
 
-        it("should return array with 3 Hello World", function () {
-            request.get(base_url+'number/3', function (error, response, body) {
+        it("should return array with length of 3", function (done) {
+            request.get(base_url + 'number/3', function (error, response, body) {
+                expect(body.length).toBe(3);
+                done();
+            });
+        });
+
+        it("should return array with 3 Hello World", function (done) {
+            request.get(base_url + 'number/3', function (error, response, body) {
                 expect(body).toEqual(["Hello World","Hello World","Hello World"]);
+                done();
             });
         });
-
     });
 });
